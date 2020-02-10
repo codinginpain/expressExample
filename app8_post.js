85070f293c4b1da8fb0bfee4ce45685884de6ced
codinginpain <51408335+codinginpain@users.noreply.github.com>
2020-02-10 09:40:13 +0900

    post.send // before rautting

--- app8_post.js
+++ app8_post.js
@@ -9,19 +9,19 @@ var app = express();
 
 app.set(`port`, process.env.PORT || 3000);
 // app.use(); //미들웨어 등록
-app.use(static(path.join(__dirname, `public`))); // path모듈에 .join해서 상위폴더와 하위폴더 경로를 이어줌 __dirname 현재 실행되는 폴더 + public
-// app.use(`/public`, static(path.join(__dirname, `public`))); 이렇게하면 public을 명시 해주어야함 / 위처럼 빼주는게 더 좋다
+app.use(static(path.join(__dirname, `public`))); // path모듈에 .join해서 상위폴더와 하위폴더 경로를 이어줌 __dirname 현재 실행되는 폴더 + public 
+// app.use(`/public`, static(path.join(__dirname, `public`))); //이렇게하면 public을 명시 해주어야함 / 위처럼 빼주는게 더 좋다
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
     console.log(`first middleware called`);
 
     var userAgent = req.header("User-Agent");
-    var paramName = req.body.name || req.query.name; //post or get
+    var paramId = req.body.id || req.query.id; //post or get -> from 태그안에 input 의 name="id" 값에 저장 된 값으로 땀
 
-    res.send(`<h3>respon from server User-Agent -> ${userAgent}</h3><h3>ParamName -> ${paramName}</h3>`)
+    res.send(`<h3>respon from server User-Agent -> ${userAgent}</h3><h3>ParamName -> ${paramId}</h3>`)
 });
 
 
@@ -29,3 +29,4 @@ var server = http.createServer(app).listen(app.get(`port`), function() {
     console.log(`server7 starts with express : ${app.get(`port`)}`);
 });
 
+//포스트맨을 사용해서 parameter를 넘겨 보았음
